af515f3f9411649307464aa5efd82acb7135b3c3
AlbertVilarnau <avilarnauillana@gmail.com>
2024-01-18 13:43:18 +0100

    Versión 2.1 del código. Mas informacion en el README

--- Blackjack.c
+++ Blackjack.c
@@ -12,38 +12,40 @@ char *values[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "
 char *suits[] = {"Hearts", "Diamonds", "Spades", "Clubs"};
 
 // Estructura para una carta
-struct card {
+struct card
+{
     char *value;
     char *suit;
     int numericValue;
 };
 
-
-
 struct card deck[TOTAL_CARDS];
 struct card playerHand[MAX_HAND];
-int numPlayerCards = 0;
-int playerSum = 0;
+int numPlayerCards = 0, playerSum = 0;
 char choice;
 
-void showCard(struct card c) {
+void showCard(struct card c)
+{
     printf("  %s of %s\n", c.value, c.suit);
-
 }
 
-int sumHand(struct card hand[], int numCards) {
+int sumHand(struct card hand[], int numCards)
+{
     int sum = 0;
     int Aces = 0;
 
-    for (int i = 0; i < numCards; i++) {
+    for (int i = 0; i < numCards; i++)
+    {
         sum += hand[i].numericValue;
 
-        if (hand[i].numericValue == 11 && strcmp(hand[i].value, "A") == 0) {
+        if (hand[i].numericValue == 11 && strcmp(hand[i].value, "A") == 0)
+        {
             Aces++;
         }
     }
 
-    while (sum > 21 && Aces > 0) {
+    while (sum > 21 && Aces > 0)
+    {
         sum -= 10;
         Aces--;
     }
@@ -51,66 +53,136 @@ int sumHand(struct card hand[], int numCards) {
     return sum;
 }
 
-int main() {
+int main()
+{
 
-    for (int i = 0; i < TOTAL_CARDS; i++) {
-        deck[i].value = values[i % CARD_VALUES];
-        deck[i].suit = suits[i / CARD_VALUES];
-        deck[i].numericValue = (i % CARD_VALUES) + 2;
-        if (deck[i].numericValue > 10) deck[i].numericValue = 10;
-        if (strcmp(deck[i].value, "A") == 0) deck[i].numericValue = 11;
-    }
+    int money, bet, out;
+    char menu;
 
-        srand(time(NULL));
-    for (int i = 0; i < TOTAL_CARDS; i++) {
-        int j = rand() % TOTAL_CARDS;
-        struct card temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
+    printf("How much money do you bring?: ");
+    scanf("%i", &money);
 
-    printf("Welcome to Blackjack!\n");
+    do
+    {
 
-    playerHand[numPlayerCards++] = deck[0];
-    playerHand[numPlayerCards++] = deck[1];
-    playerSum = sumHand(playerHand, numPlayerCards);
+        numPlayerCards = 0;
+        playerSum = 0;
+        printf("\nDo you want to play Blackjack? (Y/N): ");
+        scanf(" %c", &menu);
 
-    printf("Your cards:\n");
-    showCard(playerHand[0]);
-    showCard(playerHand[1]);
-    printf("Total amount: %d\n", playerSum);
+        switch (menu)
+        {
 
-    while (playerSum < 21) {
-        printf("Hit or stand (H/S): ");
-        scanf(" %c", &choice);
+        case 'Y':
+        case 'y':
 
-        if (choice == 'H' || choice == 'h') {
-            playerHand[numPlayerCards] = deck[numPlayerCards + 1];
-            playerSum = sumHand(playerHand, ++numPlayerCards);
+            printf("How much money do you want to bet?: ");
+            scanf("%i", &bet);
+            if (bet > money)
+            {
+                printf("You don't have that amount of money");
+                break;
+            }
+            money -= bet;
 
-            printf("New card:\n");
-            showCard(playerHand[numPlayerCards - 1]);
+            for (int i = 0; i < TOTAL_CARDS; i++)
+            {
+                deck[i].value = values[i % CARD_VALUES];
+                deck[i].suit = suits[i / CARD_VALUES];
+                deck[i].numericValue = (i % CARD_VALUES) + 2;
+                if (deck[i].numericValue > 10)
+                    deck[i].numericValue = 10;
+                if (strcmp(deck[i].value, "A") == 0)
+                    deck[i].numericValue = 11;
+            }
+
+            srand(time(NULL));
+            for (int i = 0; i < TOTAL_CARDS; i++)
+            {
+                int j = rand() % TOTAL_CARDS;
+                struct card temp = deck[i];
+                deck[i] = deck[j];
+                deck[j] = temp;
+            }
+
+            printf("Welcome to Blackjack!\n");
+
+            playerHand[numPlayerCards++] = deck[0];
+            playerHand[numPlayerCards++] = deck[1];
+            playerSum = sumHand(playerHand, numPlayerCards);
+
+            printf("Your cards:\n");
+            showCard(playerHand[0]);
+            showCard(playerHand[1]);
             printf("Total amount: %d\n", playerSum);
-        } else {
-            break;
-        }
-    }
 
-    srand(time(NULL));
-    int Dealer = rand() % 10 + 17;
-    if (playerSum == 21 && Dealer == 21) {
-        printf("Blackjack! But you draw with Dealer\n");
-    } else if(playerSum == 21 && Dealer != 21){
-        printf("Blackjack! You win the game!\nDealer's hand: %i", Dealer);
-    } else if (playerSum > 21) {
-        printf("You have gone too far. End of the game.\n");
-    } else if (playerSum < 21 && Dealer > 21){
-        printf("You win the game! Dealer got %i", Dealer);
-    } else if(playerSum > Dealer){
-        printf("You win the game! Dealer got %i, and you %i \n", Dealer, playerSum);
-    } else if(playerSum == Dealer){
-        printf("You draw with the Dealer. Dealer's hand: %i", Dealer);
-    }
+            while (playerSum < 21)
+            {
+                printf("Hit or stand (H/S): ");
+                scanf(" %c", &choice);
+
+                if (choice == 'H' || choice == 'h')
+                {
+                    playerHand[numPlayerCards] = deck[numPlayerCards + 1];
+                    playerSum = sumHand(playerHand, ++numPlayerCards);
+
+                    printf("New card:\n");
+                    showCard(playerHand[numPlayerCards - 1]);
+                    printf("Total amount: %d\n", playerSum);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            srand(time(NULL));
+            int Dealer = rand() % 10 + 17;
+            if (playerSum == 21 && Dealer == 21)
+            {
+                printf("Blackjack! But you draw with Dealer\n");
+                money += bet;
+            }
+            else if (playerSum == 21 && Dealer != 21)
+            {
+                printf("Blackjack! You win the game!\nDealer's hand: %i \n", Dealer);
+                money += bet * 2;
+            }
+            else if (playerSum > 21)
+            {
+                printf("You have gone too far. End of the game.\n");
+            }
+            else if (playerSum < 21 && Dealer > 21)
+            {
+                printf("You win the game! Dealer got %i", Dealer);
+                money += bet * 2;
+            }
+            else if (playerSum > Dealer)
+            {
+                printf("You win the game! Dealer got %i, and you %i \n", Dealer, playerSum);
+                money += bet * 2;
+            }
+            else if (playerSum == Dealer)
+            {
+                printf("You draw with the Dealer. Dealer's hand: %i \n", Dealer);
+                money += bet;
+            }
+
+            printf("\n Now, you have %i dollars", money);
+            if (money == 0)
+            {
+                out++;
+            }
+
+            break;
+
+        case 'n':
+        case 'N':
+            printf("You ended the game with %i dollars", money);
+            out++;
+        }
+
+    } while (out == 0);
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-Bienvenido a la version 2.0 del Blackjack!!
+Bienvenido a la version 2.1 del Blackjack!!
 
-En está version se ha implementado optimizacion de código al Blackjack. Pasamos unos ejemplos:
+En está version se han implementado varios sistemas al código. Pronto se implementará su optimización óptima
 
->> Se ha eliminado la función InitializeDeck, ya que solo se usaba una sola vez, y se ha implementado el código directamente. (Visto el la linía 53/59)
+>> Se ha añadido un sistema donde puedes jugar todas las veces que quieras sin tener que volver a complilar el juego, con un switch y un menu
 
->> Se ha eliminado la función ShuffleDeck, ya que solo se usaba una sola vez, y se ha implementado el código directamente. (Visto el la linía 61/67)
-
->> Incluida la libreria <string.h>, ya que es usada para comparar algunos datos (Visto el la linía 4)
-
->> Usado en strcmp para comparar cadenas (if (strcmp(deck[i].value, "A") == 0) deck[i].numericValue = 11;) (Linía 59)
-
->> Cambiado el texto "Quieres otra carta?" por "Hit or Stand", ya que es como se utilitza el lenguaje del juego original, el Blackjack
-
->> Añadido el sistema "Dealer", (Ver Línia 99/113)
-
+>> Se ha añadido un sistema de apuestas, juega para descubrirlo!
 
